bf8201fbb855ccc025e8be0b3df62fa7f7f519da
Moto Mori <kusep.moto1@gmail.com>
2020-01-29 06:19:29 +0000

    Add travis

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -xve
+
+#required packages
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install respkg
+
+#ros install
+cd ..
+git clone https://github.com/mori-maker/ros_setup_scripts_Ubuntu18.04_server.git
+cd ./ros_setup_scripts_Ubuntu18.04_server
+bash ./step0.bash
+bash ./step1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+source /opt/ros/melodic/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
